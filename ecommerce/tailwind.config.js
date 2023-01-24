ce06bea28ddc5a0da0609bc0e7abb14ea38fd719
Ananta kandel <kandelananta12@gmail.com>
2023-01-24 22:24:34 +0545

    second

---
+++ ecommerce/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      'hero-pattern': "url('./hero4.png')",
+    }
+  },
+  plugins: [],
+}
