523dd2dc2038c079a34e3bfd380b29ff43ae33cf
Ananta kandel <kandelananta12@gmail.com>
2023-01-25 20:54:51 +0545

    dayone

--- dayone/index.js
+++ dayone/index.js
@@ -1,14 +1,15 @@
 function a(){
+    document.getElementById("tips").innerHTML = "hello"
 // var amountinput = document.getElementsByid("billamount").value;
 var amountinput = document.getElementsByid("billamount").value;
 var dropdown = document.getElementById("dropdown").value;
 var numberpeople = document.getElementById("numberpeople").value;
 var total = (amountinput * dropdown)/numberpeople;
-document.getElementById("tips").innerHTML = total;
+// document.getElementById("tips").innerHTML = total;
 document.getElementById("tips").innerHTML = "hello"
 }
 // code to check 
-document.getElementById("tips").innerHTML = "hello"
+// document.getElementById("tips").innerHTML = "hello"
 
 document.getElementById("button").onclick = function(){
     a();
