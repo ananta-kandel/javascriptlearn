c321a45223c442ec28d7a34050cc7033984662e0
Ananta kandel <kandelananta12@gmail.com>
2023-01-25 21:22:52 +0545

    finaloneday

--- dayone/index.js
+++ dayone/index.js
@@ -1,18 +1,27 @@
-function a(){
-    document.getElementById("tips").innerHTML = "hello"
-// var amountinput = document.getElementsByid("billamount").value;
-var amountinput = document.getElementsByid("billamount").value;
+function calculatetips(){
+    // document.getElementById("tips").innerHTML = "hello"
+var amountinput = document.getElementById("billamount").value;
+if(amountinput== 0 || amountinput==" "){
+     var a = prompt("enter a value for Bill amount:")
+};
+//if user forget to enter the bill amount
+document.getElementById("billamount").value = a;
 var dropdown = document.getElementById("dropdown").value;
+
 var numberpeople = document.getElementById("numberpeople").value;
+
+if(dropdown == 0  && numberpeople == " "){
+    alert("enter good value");
+}
 var total = (amountinput * dropdown)/numberpeople;
-// document.getElementById("tips").innerHTML = total;
-document.getElementById("tips").innerHTML = "hello"
+finaltotal = total.toFixed(2);
+document.getElementById("tips").innerHTML = finaltotal;
 }
 // code to check 
 // document.getElementById("tips").innerHTML = "hello"
 
 document.getElementById("button").onclick = function(){
-    a();
+    calculatetips();
 };
 
 // document.getElementById("button").addEventListener("click", function() {
